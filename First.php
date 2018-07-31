ab59ef42d7cba49c30dc0e6a8f6c64f3b578584b
790516255@qq.com <790516255@qq.com>
2018-07-31 16:31:14 +0800

    first

---
+++ First.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace zyq\again;
+class First
+{
+    public static function doFirst()
+    {
+        echo 'first';
+    }
+
+    public static function doSecond()
+    {
+        echo 'second';
+    }
+}
