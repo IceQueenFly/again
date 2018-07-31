4f51c91cf47cd2c1b46f3f531bd1f41caa880ca6
790516255@qq.com <790516255@qq.com>
2018-07-31 16:34:14 +0800

    second

---
+++ Second.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace zyq\again;
+class Second
+{
+    public static function doOne()
+    {
+        echo 'one';
+    }
+
+    public static function doTwo()
+    {
+        echo 'two';
+    }
+}
